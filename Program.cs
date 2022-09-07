429885815b8504b46663af4a40fc4e3eb06fd15a
PhamKhanhDuy27 <phamkhanhduy0207@gmail.com>
2022-09-07 21:57:13 +0700

    Add

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LearningCS;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 2; number <= 20; number++)
+        {
+            int divisorCount = 0;
+            for (int i = 2; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisorCount++;
+                }
+            }
+
+            if (divisorCount == 0)
+            {
+                Console.WriteLine($"{number} la so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine($"{number} khong phai la so nguyen to");
+            }
+        }
+    }
+}
