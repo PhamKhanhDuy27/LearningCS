0984e191f8e56edb1b5c43d7ea319aebba0fbeb1
PhamKhanhDuy27 <phamkhanhduy0207@gmail.com>
2022-09-08 22:23:30 +0700

    Kiem tra so hoan hao

--- Program.cs
+++ Program.cs
@@ -5,22 +5,21 @@ class Program
     {
         for (int number = 2; number <= 20; number++)
         {
-            int divisorCount = 0;
-            for (int i = 2; i <= number / 2; i++)
+            int sum = 0;
+            for (int i = 1; i <= number / 2; i++)
             {
                 if (number % i == 0)
                 {
-                    divisorCount++;
+                    sum += i;
                 }
             }
-
-            if (divisorCount == 0)
+            if (sum == number)
             {
-                Console.WriteLine($"{number} la so nguyen to");
+                Console.WriteLine($"{number} la so hoan hao");
             }
             else
             {
-                Console.WriteLine($"{number} khong phai la so nguyen to");
+                Console.WriteLine($"{number} khong phai la so hoan hao");
             }
         }
     }
